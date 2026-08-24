b38cf922011c69d51141d94b30fabfd6469d4d3b
DatatestOficial <12110669zoro@gmail.com>
2026-08-24 15:03:27 -0600

    fijar valor

--- dashboard.py
+++ dashboard.py
@@ -974,8 +974,8 @@ st.markdown(f"""
         <p style="font-size: 1.2em; color: {AMARILLO}; margin-top: 0px; font-weight: bold;">
             Reporte del {hoy} con Información actualizada al <span style="color: {VERDE}; font-weight: bold;">{fecha_datos}</span> 
         </p>
-    """, unsafe_allow_html=True)
     </div>
+    """, unsafe_allow_html=True)
 
 # ═══════════════════════════════════════════════════════════════════════════════
 # Definir usuarios y accesos
@@ -1001,7 +1001,7 @@ if oref_asignada:
 with st.sidebar:
     st.header("Filtros de información")
     if tipo == 'administrador':
-        proceso = st.selectbox("Seleccionar proceso", ["NACIONAL", "8 OREF", "25 OREF", "FASE 1", "FASE 2"],index = 4)
+        proceso = st.selectbox("Seleccionar proceso", ["NACIONAL", "8 OREF", "25 OREF", "FASE 1", "FASE 2"], index=4)
     else:
         proceso = None
 
